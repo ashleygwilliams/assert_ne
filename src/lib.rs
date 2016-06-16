64a15e3317da2656c63b3b921d38bc5d88a94c9a
Ashley Williams <ashley666ashley@gmail.com>
2016-06-16 19:35:19 +0100

    remove unecessary parans

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ macro_rules! assert_ne {
     ($left:expr , $right:expr) => ({
         match (&$left, &$right) {
             (left_val, right_val) => {
-                if (*left_val == *right_val) {
+                if *left_val == *right_val {
                     panic!("assertion failed: `(left !== right)` \
                            (left: `{:?}`, right: `{:?}`)", left_val, right_val)
                 }
