44de29c53acebb6b9b07bc4b4a58d9e53296cca3
Ashley Williams <ashley666ashley@gmail.com>
2016-06-16 19:01:29 +0100

    implement not equals

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-    }
+/// Asserts that two expressions are not equal to each other.
+///
+/// On panic, this macro will print the values of the expressions with their
+/// debug representations.
+///
+/// # Examples
+///
+/// ```
+/// let a = 3;
+/// let b = 2 + 2;
+/// assert_ne!(a, b);
+/// ```
+#[macro_export]
+macro_rules! assert_ne {
+    ($left:expr , $right:expr) => ({
+        match (&$left, &$right) {
+            (left_val, right_val) => {
+                if (*left_val == *right_val) {
+                    panic!("assertion failed: `(left !== right)` \
+                           (left: `{:?}`, right: `{:?}`)", left_val, right_val)
+                }
+            }
+        }
+    })
 }
