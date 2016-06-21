95c5f40a18ce83abde97b0e30e005c8258ce8f05
Ashley Williams <ashley666ashley@gmail.com>
2016-06-21 11:48:01 -0400

    update to reflect new custom msg feature in std #33976

--- src/lib.rs
+++ src/lib.rs
@@ -23,17 +23,34 @@ macro_rules! assert_ne {
                 }
             }
         }
-    })
+    });
+    ($left:expr , $right:expr, $($arg:tt)*) => ({
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if *left_val == *right_val {
+                    panic!("assertion failed: `(left != right)` \
+                           (left: `{:?}`, right: `{:?}`): {}", left_val, right_val,
+                           format_args!($($arg)*))
+                }
+            }
+        }
+    });
 }
 
 #[test]
 #[should_panic]
 fn it_panics_when_the_values_are_equal() {
-  assert_ne!(3, 3);
+    assert_ne!(3, 3);
 }
 
 #[test]
 fn it_does_not_panic_when_the_values_are_not_equal () {
-  assert_ne!(3, 4);
+    assert_ne!(3, 4);
 }
 
+#[test]
+fn it_should_allow_a_custom_message () {
+    assert_ne!(42, 43, "foo bar");
+    assert_ne!(42, 43, "a {} c", "b");
+    assert_ne!(42, 43, "{x}, {y}, {z}", x = 1, y = 2, z = 3);
+}
